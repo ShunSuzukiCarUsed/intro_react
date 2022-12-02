d319cbc1bbf9169407b39c79ec81756dc02eaca0
ShunSuzukiCarUsed <suzuki@carused.jp>
2022-12-02 14:01:45 +0900

    board constracuot

--- src/index.js
+++ src/index.js
@@ -24,11 +24,17 @@ function Square(props) {
 // }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
     renderSquare(i) {
         return <Square
             value={this.props.squares[i]}
             onClick={() => this.props.onClick(i)}
-        /> 
+        />
     }
     render() {
         return (
